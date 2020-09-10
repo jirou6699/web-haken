499934385f2c3167e63f8863614a7a7fd992b32b
jirou6699 <shundom09@gmail.com>
2020-09-10 22:05:46 +0900

    Add before action at matches controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,8 +1,16 @@
 class MatchesController < ApplicationController
+  before_action :move_to_index, except: [:index]
+  
   def index
     @matches = Job.all
   end
 
   def show
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
