409adc44f16979d1cf7b2c05542c71b330f72f75
jirou6699 <shundom09@gmail.com>
2020-09-11 15:57:14 +0900

    Add review model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :reviews
+  
   validates :name, presence: true, uniqueness: true
   validates :nickname, presence: true, uniqueness: true
 
