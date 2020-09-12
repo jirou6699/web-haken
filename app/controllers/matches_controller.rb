3cfc802db8b2ce15809b500ae4d4c5a5570dc7e2
jirou6699 <shundom09@gmail.com>
2020-09-12 20:48:20 +0900

    Add form with  review match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -8,7 +8,7 @@ class MatchesController < ApplicationController
   def show
     @match = Job.find(params[:id])
     @review = Review.new
-    @reviews = @match.reviews.inclues(:user)
+    @reviews = @match.reviews.includes(:user)
   end
 
   def search
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root "matches#index"
   resources :matches, only: [:show] do
+    resources :reviews, only: [:create]
     collection do
       get 'search'
     end 
   end
-  resources :jobs, only: [:index, :new, :create, :destroy, :edit, :update] do
-    resources :reviews, only: :create
-  end
+  resources :jobs, only: [:index, :new, :create, :destroy, :edit, :update]
 end
