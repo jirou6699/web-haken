92303246430ac9db28c8a10cc727711367f98a7d
jirou6699 <shundom09@gmail.com>
2020-09-12 11:24:32 +0900

    Add reviews controller create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  def create
+    review = Review.create(review_params)
+    redirect_to "/matches/#{job.id}"  # ここは少し変更が必要かも？#
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:good, :bad).merge(user_id: current_user.id, job_id: params[:job_id])
+  end
 end
