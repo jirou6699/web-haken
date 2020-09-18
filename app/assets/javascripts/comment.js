d787bbd4ef6fc8bd3513223480761314a90193bd
jirou6699 <shundom09@gmail.com>
2020-09-18 20:28:08 +0900

    Add-Ajax code at comment.js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,7 +1,16 @@
 $(function(){
-  // $('#new-comment').on('submit', function(e){
-  //   e.preventDefault();
-  //   console.log(this)
-  //   let formData = new FormData(this);
-  // })
+  $('#new-comment').on('submit', function(e){
+    e.preventDefault();
+    console.log(this)
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
 });
