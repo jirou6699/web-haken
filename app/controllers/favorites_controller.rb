0606569ac1075285433f3d6c1c7fb4bcbd734cbe
jirou6699 <shundom09@gmail.com>
2020-09-21 13:51:29 +0900

    Add favorite on off

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
-    @favorite = current_user.favorites.create(job_id: params[job_id])
+    @favorite = current_user.favorites.create(job_id: params[:job_id])
     redirect_back(fallback_location: root_path)
   end
   
   def destroy
-    @job = Job.find(params [:job_id])
+    @job = Job.find(params[:job_id])
     @favorite = current_user.favorites.find_by(job_id: @job.id)
     @favorite.destroy
     redirect_back(fallback_location: root_path)
