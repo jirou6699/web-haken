6d9cf0a447f920a22dba6f0a5e990330cba19c1d
jirou6699 <shundom09@gmail.com>
2020-09-23 01:05:07 +0900

    Change js.haml

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,21 @@
 class FavoritesController < ApplicationController
-  before_action :job_item
+  before_action :set_action
 
   def create
     @favorite = current_user.favorites.create(job_id: params[:job_id])
+    @match = Job.find(params[:job_id])
   end
   
   def destroy
-    # @job = Job.find(params[:job_id])
-    @favorite = current_user.favorites.find_by(job_id: @job.id)
+    @match = Job.find(params[:job_id])
+    @favorite = current_user.favorites.find_by(job_id: @match.id)
     @favorite.destroy
   end
 
   private
-  def job_item
+
+  def set_action
     @job = Job.find(params[:job_id])
   end
+
 end
