5d8704cd78097b373ff42334cc559e8efb965348
jirou6699 <shundom09@gmail.com>
2020-09-24 00:22:11 +0900

    Add Footer scss haml

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -2,7 +2,7 @@ $(function() {
   $(".slider").slick({
     dots: true,
     autoplay: true, // 自動再生を設定
-    autoplaySpeed: 2000, // 自動再生のスピード（ミリ秒単位）
+    autoplaySpeed: 1500, // 自動再生のスピード（ミリ秒単位）
     dots: true, // ドットインジケーターの表示
     speed: 3000,
   });
