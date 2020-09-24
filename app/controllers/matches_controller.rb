7ce086e9b546bc20ba7042d99c44475e41b9875b
jirou6699 <shundom09@gmail.com>
2020-09-24 23:35:24 +0900

    Add code at matches_controller.rb and job_matches.html.haml

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,8 +1,7 @@
 class MatchesController < ApplicationController
   
   def index
-    @matches = Job.all.order("created_at DESC")
-    @matches = Job.all.page(params[:page]).per(5)
+    @matches = Job.all.order("created_at DESC").page(params[:page]).per(3)
   end
 
   def show
