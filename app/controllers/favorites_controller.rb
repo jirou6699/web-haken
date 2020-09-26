dfff2c23ce017088cbbae0bbd5de357f98d7d7b6
jirou6699 <shundom09@gmail.com>
2020-09-26 22:15:07 +0900

    Delete favorite Ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,19 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :job_item
 
   def create
     @favorite = current_user.favorites.create(job_id: params[:job_id])
-    @match = Job.find(params[:job_id])
+    redirect_back(fallback_location: root_path)
   end
   
   def destroy
-    @match = Job.find(params[:job_id])
-    @favorite = current_user.favorites.find_by(job_id: @match.id)
+    @favorite = current_user.favorites.find_by(job_id: @job.id)
     @favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def job_item
+    @job = Job.find(params[:job_id])
   end
 end
