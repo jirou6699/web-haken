8bd4640add9d5d0ab44a3fb65cb6afa28804905e
jirou6699 <shundom09@gmail.com>
2020-09-27 17:48:03 +0900

    Change Ajax cole for favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,20 @@
 class FavoritesController < ApplicationController
-  before_action :job_item
+  before_action :authenticate_user!
+  before_action :set_tweet
 
   def create
-    @favorite = current_user.favorites.create(job_id: params[:job_id])
-    redirect_back(fallback_location: root_path)
+    @favorite = current_user.favorites.create(job_id: @job.id)
+    # redirect_back(fallback_location: root_path) 
   end
   
   def destroy
     @favorite = current_user.favorites.find_by(job_id: @job.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   private
-  def job_item
+  def set_tweet
     @job = Job.find(params[:job_id])
   end
 end
--- db/migrate/20200921025036_create_favorites.rb
+++ db/migrate/20200921025036_create_favorites.rb
@@ -1,8 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[6.0]
   def change
     create_table :favorites do |t|
-      t.references :job, foreign_key: true
-      t.references :user, foreign_key: true
+      t.integer :job_id
+      t.integer :user_id
       
       t.timestamps
     end
