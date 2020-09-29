0c1508a5108eb9297636b9b90a8f4d4c2d9e247b
jirou6699 <shundom09@gmail.com>
2020-09-30 00:16:36 +0900

    Add css hover at view file

---
+++ db/migrate/20200928165713_remove_rate_to_review.rb
@@ -0,0 +1,4 @@
+class RemoveRateToReview < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_025036) do
+ActiveRecord::Schema.define(version: 2020_09_28_165713) do
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "job_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_025036) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "rate"
     t.index ["job_id"], name: "index_reviews_on_job_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
