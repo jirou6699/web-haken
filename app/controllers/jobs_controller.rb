bdc61c91415761c97f91d64e28e38b2aedf2592e
jirou6699 <shundom09@gmail.com>
2020-10-02 17:38:57 +0900

    Add basic auth at job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
-  
+  # before_action :basic_auth
+
   def index
     @job = Job.new
     @lists = Job.all.order("created_at DESC")
@@ -31,6 +32,12 @@ class JobsController < ApplicationController
 
   def job_params
     params.require(:job).permit(:title, :occupation, :image1, :image2, :image3, :description, :salary, :access, :time, :week, :access, :emplyee, :term, :pr1, :pr2, :pr3)
+  end
+
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == 'jobs' && password == '000000'
+  #   end
   end
 
 end
